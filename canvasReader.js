f86dd9f1acddc0d83570d992bb3781005a5568c9
Sina <sadeghesfahani.sina@gmail.com>
2024-11-12 09:40:21 +0100

    these codes will be used to incpect what is going on in the webpage of quicktron for furture developements

---
+++ canvasReader.js
@@ -0,0 +1,23 @@
+const canvas = document.querySelector('canvas');
+
+
+const getFunctions = ()=>{
+    return Object.keys(window).filter(key => typeof window[key] === 'function');
+}
+
+
+const runFunction = (functionName, args = []) => {
+    if (typeof window[functionName] === 'function') {
+        return window[functionName](...args); // Spread args to handle multiple arguments
+    } else {
+        console.error(`Function "${functionName}" does not exist in the global scope.`);
+    }
+}
+
+
+// this is for inspect data that are comming from websocket
+const originalWebSocket = WebSocket.prototype.send;
+WebSocket.prototype.send = function(data) {
+    console.log("WebSocket data:", data); // Inspect incoming data
+    originalWebSocket.apply(this, arguments);
+};
